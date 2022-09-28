96aeec36ea2ee64364e689445a91812d106e3ef0
carmelete96 <carmenortegarico@gmail.com>
2022-09-28 18:01:56 +0200

    feat: remove tracks from a playlist

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -21,6 +21,7 @@ class App extends React.Component {
           { name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6 }],
       };
       this.addTrack = this.addTrack.bind(this);
+      this.removeTrack = this.removeTrack.bind(this);
     }
 
     addTrack(track) {
@@ -32,15 +33,23 @@ class App extends React.Component {
       this.setState({playlistTracks: tracks});
     }
 
+    removeTrack(track) {
+      console.log('hola', track);
+      let tracks = this.state.playlistTracks;
+      tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+      console.log(tracks);
+      this.setState({playlistTracks: tracks});
+    }
+
     render () {
       return (
         <div>
           <h1>Ja<span className="highlight">mmm</span>ing</h1>
           <div className="App">
-            {/*<SearchBar />*/}
+            <SearchBar />
             <div className="App-playlist">
-              <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+              <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
             </div>
           </div>
         </div>
