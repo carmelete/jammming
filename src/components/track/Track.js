48b04ff8ff607782f6cd110e84816c424f490d23
carmelete96 <carmenortegarico@gmail.com>
2022-09-28 11:49:25 +0200

    feat: create track component and styles

---
+++ src/components/track/Track.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Track extends React.Component {
+    renderAction () {
+        <button className="Track-action">{ isRemoval ? '-' : '+' }</button>
+    }
+
+    render () {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3><!-- track name will go here --></h3>
+                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                </div>
+                <button class="Track-action">{ this.renderAction }</button>
+            </div>
+        )
+    }
+}
+
+export default Track;
