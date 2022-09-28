1071b4125b4cf7b03ee391e554a267b16dba03d7
carmelete <carmenortegarico@gmail.com>
2022-09-28 11:58:44 +0200

    App (#1)
    * feat: app structure and styles
    
    * fix: import React

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,20 @@
+import React from 'react';
 import './App.css';
+import SearchBar from '../searchBar/SearchBar';
+import SearchResults from '../searchResults/SearchResults';
+import Playlist from '../playlist/Playlist';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          <Playlist />
+        </div>
+      </div>
     </div>
   );
 }
