b7c0406ac1fcfed46619d18eabab3de94a0060d3
carmelete96 <carmenortegarico@gmail.com>
2022-09-29 10:08:40 +0200

    feat: change the name of a playlist

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import SearchBar from '../searchBar/SearchBar';
 import SearchResults from '../searchResults/SearchResults';
 import Playlist from '../playlist/Playlist';
-import TrackList from '../trackList/TrackList';
 
 class App extends React.Component {
     constructor (props) {
@@ -22,6 +21,7 @@ class App extends React.Component {
       };
       this.addTrack = this.addTrack.bind(this);
       this.removeTrack = this.removeTrack.bind(this);
+      this.updatePlaylistName = this.updatePlaylistName.bind(this);
     }
 
     addTrack(track) {
@@ -41,6 +41,10 @@ class App extends React.Component {
       this.setState({playlistTracks: tracks});
     }
 
+    updatePlaylistName (name) {
+      this.setState({playlistName: name });
+    }
+
     render () {
       return (
         <div>
@@ -49,7 +53,7 @@ class App extends React.Component {
             <SearchBar />
             <div className="App-playlist">
               <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
             </div>
           </div>
         </div>
